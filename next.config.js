8b7cedd22ae90dc76dd103ec3c955a09b5a3c106
shifuguru <buffd.nz@gmail.com>
2026-04-13 20:40:38 +1200

    updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repo = 'buffdnz-web';
+const repo = 'buffdnz';
 
 const nextConfig = {
   output: 'export',
